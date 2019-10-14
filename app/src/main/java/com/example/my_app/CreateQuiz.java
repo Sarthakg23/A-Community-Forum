0b186382202885fc6a4b8310df5c434f5d585ecf
sarthakg23 <sarthakg23@gmail.com>
2019-10-14 23:15:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/my_app/CreateQuiz.java
@@ -0,0 +1,14 @@
+package com.example.my_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_quiz);
+    }
+}
---
+++ app/src/main/java/com/example/my_app/PlayQuiz.java
@@ -0,0 +1,14 @@
+package com.example.my_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_quiz);
+    }
+}
